6e493d786d5699390138b1f3621464a92d8a92d6
titantooth <ali.rabieeie@gmail.com>
2023-08-20 12:51:59 +0330

    started

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <section className=" bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-20"></section>
+      </main>
+    </>
+  );
+}
